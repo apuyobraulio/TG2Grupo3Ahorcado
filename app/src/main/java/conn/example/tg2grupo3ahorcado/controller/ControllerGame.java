e06dacf92296eb8bb187c5d45d4faccaa35d3591
Raúl Moya Gimeno <rmoya889@gmail.com>
2023-01-26 17:15:40 +0100

    Controlador del juego

--- app/src/main/java/conn/example/tg2grupo3ahorcado/controller/ControllerGame.java
+++ app/src/main/java/conn/example/tg2grupo3ahorcado/controller/ControllerGame.java
@@ -1,4 +1,36 @@
 package conn.example.tg2grupo3ahorcado.controller;
 
+import java.util.ArrayList;
+
 public class ControllerGame {
+
+    private final String palabra;
+    private int vidas;
+
+    public ControllerGame(String palabra){
+        this.palabra = palabra;
+        this.vidas = 6;
+    }
+
+    public boolean introducirPalabra(String intento){
+        if(this.palabra.equals(intento)){
+            return true;
+        }
+        vidas--;
+        return false;
+    }
+
+    public ArrayList<Integer> introducirLetra(char letra){
+        ArrayList<Integer> posiciones = new ArrayList<>();
+        char[] letras = palabra.toCharArray();
+        for (int i = 0; i < letras.length; i++) {
+            if (letra == letras[i]) posiciones.add(i);
+        }
+        if (posiciones.isEmpty()) vidas--;
+        return posiciones;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
 }
