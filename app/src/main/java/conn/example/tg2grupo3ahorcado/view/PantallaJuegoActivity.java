ae22d1b3dbad7c860ea2e6638f3286ed8de08831
Alvaro <alvaropuyobraulio@gmail.com>
2023-01-27 17:33:47 +0100

    raya

--- app/src/main/java/conn/example/tg2grupo3ahorcado/view/PantallaJuegoActivity.java
+++ app/src/main/java/conn/example/tg2grupo3ahorcado/view/PantallaJuegoActivity.java
@@ -2,6 +2,7 @@ package conn.example.tg2grupo3ahorcado.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,12 +19,19 @@ public class PantallaJuegoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_juego);
 
         TextView txtfallos= findViewById(R.id.txtfallos);
+
         TextView txtletra1= findViewById(R.id.txtletra1);
+        txtletra1.setPaintFlags(txtletra1.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         TextView txtletra2= findViewById(R.id.txtletra2);
+        txtletra2.setPaintFlags(txtletra2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         TextView txtletra3= findViewById(R.id.txtletra3);
+        txtletra3.setPaintFlags(txtletra3.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         TextView txtletra4= findViewById(R.id.txtletra4);
+        txtletra4.setPaintFlags(txtletra4.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         TextView txtletra5= findViewById(R.id.txtletra5);
+        txtletra5.setPaintFlags(txtletra5.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         TextView txtletra6= findViewById(R.id.txtletra6);
+        txtletra6.setPaintFlags(txtletra6.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         EditText txtpalabra= findViewById(R.id.txtpalabra);
         Button btncomprobar= findViewById(R.id.btncomprobar);
 
